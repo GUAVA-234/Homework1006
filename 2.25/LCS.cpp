7175c32387d416b370c05642d4498b82b090785a
GUAVA-234 <B0910296646@gmail.com>
2023-10-12 18:04:24 +0800

    hotfix

---
+++ 2.25/LCS.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void L()
+{	
+	for(int o=0;o<5;o++)
+	{
+		printf("*"); 
+		printf("\n");
+	}
+	for(int i=0;i<8;i++)printf("*"); 
+	printf("\n");
+}
+void C()
+{
+ for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (i == 0 || i == 7 - 1) {
+                printf("*");
+            }
+            else if (j == 0) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+void S()
+{
+int i, j;
+
+
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7; j++) {
+            if ((i == 0 || i == 3 || i == 6) && j < 6) {
+                printf("*");
+            } else if ((i > 0 && i < 3) && (j == 0)) {
+                printf("*");
+            } else if ((i > 3 && i < 6) && (j == 5)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+	L();
+	C();
+	S();
+	
+	return 0;
+}
